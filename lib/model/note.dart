20a660fb9a3a597add6822414de5f2b1fbb89cf2
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-28 22:18:14 +0300

    tablo sutunları oluşturuldu

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,3 +1,15 @@
+final String tableNotes='notes';
+//veri tabanındaki stun adlarını tanımlar
+class NoteFields{
+  static final String id='_id';
+  static final String isImportant='isImportant';
+  static final String number='number';
+  static final String title='title';
+  static final String description='description';
+  static final String time='time';
+}
+
+
 class Note {
   final int? id;
   final bool isImportant;
