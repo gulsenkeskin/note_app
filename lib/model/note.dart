3fe7860ef362ed4debe66a2cab32fb8e0f750201
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-28 22:55:58 +0300

    Update note.dart

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -26,4 +26,13 @@ class Note {
     required this.description,
     required this.createdTime,
   });
+
+  Map<String, Object?> toJson()=>{
+    NoteFields.id:id,
+    NoteFields.title:title,
+    NoteFields.isImportant:isImportant ? 1 :0,
+    NoteFields.number:number,
+    NoteFields.description:description,
+    NoteFields.time:createdTime.toIso8601String(),
+  };
 }
