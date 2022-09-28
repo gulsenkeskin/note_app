9b3af40084c8aa510f10575a2999f41eef9ad8ac
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-28 22:12:04 +0300

    Update note.dart

---
+++ lib/model/note.dart
@@ -0,0 +1,17 @@
+class Note {
+  final int? id;
+  final bool isImportant;
+  final int number;
+  final String title;
+  final String description;
+  final DateTime createdTime;
+
+  const Note({
+    this.id,
+    required this.isImportant,
+    required this.number,
+    required this.title,
+    required this.description,
+    required this.createdTime,
+  });
+}
