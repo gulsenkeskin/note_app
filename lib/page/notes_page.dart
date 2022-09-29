e7bff95211fd4595e1ea7270393880b76640fd91
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 10:17:41 +0300

    Update notes_page.dart

--- lib/page/notes_page.dart
+++ lib/page/notes_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:note_app/db/notes_database.dart';
 import 'package:note_app/model/note.dart';
+import 'package:note_app/page/note_detail_page.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({Key? key}) : super(key: key);
@@ -35,11 +37,52 @@ class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title:const Text(
+          title: const Text(
             'Notes',
             style: TextStyle(fontSize: 24),
           ),
           actions: [],
         ),
+        body: Center(
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : notes.isEmpty
+                  ? const Text(
+                      'Mevcut Notunuz Yok',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  : buildNotes(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          child: Icon(Icons.add),
+          onPressed: () async {
+            //Todo
+            // await Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ))
+            refreshNotes();
+          },
+        ),
+      );
+
+  Widget buildNotes() => StaggeredGridView.countBuilder(
+        padding: const EdgeInsets.all(8),
+        itemCount: notes.length,
+        staggeredTileBuilder: (index) => const StaggeredTile.fit(2),
+        crossAxisCount: 4,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        itemBuilder: (context, index) {
+          final note = notes[index];
+
+          return GestureDetector(
+            onTap: () async {
+              //TODO
+              await Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => NoteDetailPage()));
+              refreshNotes();
+            },
+            //todo,
+          );
+        },
       );
 }
