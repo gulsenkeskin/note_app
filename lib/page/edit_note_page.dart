e9dcb9a7891d541e94ae12a8dc8e9ba55753f50d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 10:17:47 +0300

    Update edit_note_page.dart

---
+++ lib/page/edit_note_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditNotePage extends StatefulWidget {
+  const EditNotePage({Key? key}) : super(key: key);
+
+  @override
+  State<EditNotePage> createState() => _EditNotePageState();
+}
+
+class _EditNotePageState extends State<EditNotePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
