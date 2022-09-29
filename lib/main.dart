ca01e11656f3519d712950c40a05413d3e9f75cc
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 13:22:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    themeMode: ThemeMode.dark,
-    theme: ThemeData(
-      primaryColor: Colors.black,
-      scaffoldBackgroundColor: Colors.blueGrey.shade900,
-      appBarTheme: AppBarTheme(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-    ),
-    home: NotesPage(),
-  );
+        debugShowCheckedModeBanner: false,
+        title: title,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        home: NotesPage(),
+      );
 }
