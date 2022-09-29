93831dc10c7902495b792e9ec175188cedf488c9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 09:51:57 +0300

    Create notes_page.dart

---
+++ lib/page/notes_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/db/notes_database.dart';
+import 'package:note_app/model/note.dart';
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({Key? key}) : super(key: key);
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  late List<Note> notes;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshNotes();
+  }
+
+  @override
+  void dispose() {
+    NotesDatabase.instance.close();
+    super.dispose();
+  }
+
+
+  Future refreshNotes() async {
+    setState(() => isLoading = true);
+    //veri tabanından notları çeker :)
+    this.notes = await NotesDatabase.instance.readAllNotes();
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
