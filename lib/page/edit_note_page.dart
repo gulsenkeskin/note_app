23a8bf7587f999741fa534b395ae37af72fdf71d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 13:49:51 +0300

    Update edit_note_page.dart

--- lib/page/edit_note_page.dart
+++ lib/page/edit_note_page.dart
@@ -54,14 +54,14 @@ class _AddEditNotePageState extends State<AddEditNotePage> {
     final isFormValid = title.isNotEmpty && description.isNotEmpty;
 
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      padding:const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           onPrimary: Colors.white,
           primary: isFormValid ? null : Colors.grey.shade700,
         ),
         onPressed: addOrUpdateNote,
-        child: Text('Save'),
+        child:const Text('Save'),
       ),
     );
   }
